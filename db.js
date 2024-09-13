d9de442a5a81e51e934affa0203f606a9a0114f5
Udit-saini <uditsaini86072@gmail.com>
2024-09-13 03:17:47 -0700

    next version of db derver

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose');
 //it is mongodb connection url
 //const mongoUrl='mongodb://localhost:27017/myDatabase';
-const mongoUrl='mongodb+srv://uditsaini86072:Saini1234@cluster0.gaa43.mongodb.net/';
+require('dotenv').config();
+const mongoUrl=process.env.MONGODB_URL;
 //set up mongodb connsection
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 //defines the db object
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express()
 const db=require('./db')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
-
+require('dotenv').config();
+// const PORT=process.env.PORT||3002;
 
 
 const AdharRoutes=require('./routes/AdharRoutes')
@@ -13,4 +14,9 @@ app.listen(3001)
 const personRoutes = require('./routes/personRoutes');
 app.use('/Person',personRoutes);
 app.listen(3000)
+
+
+// app.listen(PORT,()=>{
+//     console.log('listen on 3000'); 
+// })
 //hi this comment is added for testing purpose
