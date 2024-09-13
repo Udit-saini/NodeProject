43e65749643c2a75034c22a5ea160a8ab3141dea
Udit-saini <uditsaini86072@gmail.com>
2024-09-12 20:31:13 -0700

    next version of db derver

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 //it is mongodb connection url
-const mongoUrl='mongodb://localhost:27017/myDatabase';
-
+//const mongoUrl='mongodb://localhost:27017/myDatabase';
+const mongoUrl='mongodb+srv://uditsaini86072:Saini1234@cluster0.gaa43.mongodb.net/';
 //set up mongodb connsection
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 //defines the db object
